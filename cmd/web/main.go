7397716af7a922fe391d47ccf5b4915ffb2a1a8e
Giully314 <giully@protonmail.com>
2025-02-19 23:38:01 +0100

    implement http file server.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,9 @@ import (
 
 
 func main() {
+	// Create a http server which serves files from ui/static.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	// Create a router for the application (usually there is only one)
 	// which is responsible for dispatch the requests.
 	// Method based routing allows to specify the type of http request to 
@@ -16,6 +19,7 @@ func main() {
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	log.Print("Starting server on :4000")
 
 	// Use the default http server for this basic example.
